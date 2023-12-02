fc9f57e34f618d8e2ed78692aadab6ac2ebc0eaa
Belseir <ffutryk52@gmail.com>
2023-12-02 03:36:03 -0300

    feat(2023): add day 02 solution

---
+++ src/2023/day-02/solution.ts
@@ -0,0 +1,56 @@
+type Color = "red" | "green" | "blue"
+
+const partOne = (input: string): number => {
+    const SEARCHING = {
+        red: 12,
+        green: 13,
+        blue: 14
+    }
+
+    return input.split("\n").reduce((acc, game, id) => {
+        const sets = game.replace(/Game [0-9]*: /g, "").split(";")
+
+        const isValidGame = sets.every((set) => {
+            const pulls = set.split(",").reduce((acc, pull) => {
+                const [amount, color] = pull.trim().split(" ")
+
+                return {
+                    ...acc,
+                    [color]: Number(amount)
+                }
+            }, {})
+
+            return Object.entries(pulls).every(([color, value]) => {
+                return (value as number) <= SEARCHING[color as Color]
+            })
+        })
+
+        return isValidGame ? acc + id + 1 : acc
+    }, 0)
+}
+
+const partTwo = (input: string): number => {
+    return input.split("\n").reduce((acc, game) => {
+        const minimum = {
+            red: 0,
+            green: 0,
+            blue: 0
+        }
+
+        game.replace(/Game [0-9]*: /g, "")
+            .split(";")
+            .forEach((set) => {
+                set.split(",").forEach((cube) => {
+                    const [amount, color] = cube.trim().split(" ")
+
+                    if (Number(amount) > minimum[color as Color]) {
+                        minimum[color as Color] = Number(amount)
+                    }
+                })
+            })
+
+        return acc + minimum.red * minimum.green * minimum.blue
+    }, 0)
+}
+
+export { partOne, partTwo }
